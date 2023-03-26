82d3245c4d68108e14e68660f95ef68b4986b4e5
David Sword <vexiconlieus@gmail.com>
2023-03-25 20:21:23 -0700

    Updated connect js by remvoign async

--- db/connect.js
+++ db/connect.js
@@ -2,19 +2,19 @@ const dotenv = require('dotenv')
 dotenv.config();
 let uri = process.env['MONGODB_URI'];
 const MongoClient = require('mongodb').MongoClient;
- async function initDb(uri) {
+  function initDb(uri) {
   let mongoClient;
-  
   try {
       mongoClient = new MongoClient(uri);
       console.log('Connecting to MongoDB Atlas cluster...');
-      await mongoClient.connect();
+       mongoClient.connect();
       console.log('Successfully connected to MongoDB Atlas!');
       return mongoClient;
   } catch (error) {
       console.error('Connection to MongoDB Atlas failed!', error);
       process.exit();
   }
+
 }
 let mongoClient = initDb(uri);
 
@@ -24,6 +24,7 @@ const getDb = () => {
   }
   return initDb(uri);
 };
+
 module.exports = {
   initDb,
   getDb,
